27968f0895cb9aefa83778aecff4141001e729b7
Kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2019-04-16 14:07:05 +0530

    Implement basic express server that serves welcome message.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.url, req.method);
+  next();
+});
+
+app.get("/greeting", (req, res) => {
+  res.send({ message: "Welcome to the page" });
+});
+
+app.listen(8000, () => console.log("listening at port 8000"));
