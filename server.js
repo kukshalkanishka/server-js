198a5356f835545dd02032ab7137af7ccd4c3924
Kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2019-04-16 15:35:31 +0530

    Modify server.

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res) => res.send("<h1> helllloooo</h1>"));
+
 app.get("/greeting", (req, res) => {
   res.send({ message: "Welcome to the page" });
 });
 
-app.use(express.static("client/build/static"));
+// app.use(express.static("client/build/static"));
 
 app.listen(8000, () => console.log("listening at port 8000"));
